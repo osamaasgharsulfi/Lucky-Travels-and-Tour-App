72624b5eba031226caf337d8bcdb7355e39f3e0b
osamaasgharsulfi <asgharusama4@gmail.com>
2022-07-18 11:03:56 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+app.use(express.json());
+
+
+
+
+const tours = JSON.parse( fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours/:id', (req, res) =>{
+
+    console.log(req.params);
+    
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id)
+    
+    // if(id > tours.length)
+    if(!tour)
+    {
+        return res.status(404).json({
+            status:'fail',
+            message:'Invalid ID'
+        }); 
+    }
+    
+    res.status(200).json({
+        status: 'success',
+        data:{
+            tour
+        }
+    })
+});
+
+app.post('/api/v1/tours', (req, res) => {
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour)   
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours) , err => {
+        res.status(201).json({
+            status: 'sucess',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+})
+
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+     if(req.params.id * 1 > tours.length)
+     {
+         return res.status(404).json({
+             status:'fail',
+             message:'Invalid ID'
+         }); 
+     }
+
+     res.status(204).json({
+        status: 'success',
+        data: null 
+     })
+
+})
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App runing on the port ${port}....`);
+})
+
